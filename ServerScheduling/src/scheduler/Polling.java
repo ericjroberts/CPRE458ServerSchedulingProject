34d5f7c0bffbaf8d5df33c16bd00a14ce3542ed6
cjlougee <cjlougee@gmail.com>
2015-12-06 21:11:32 -0600

    Polling server

--- ServerScheduling/src/scheduler/Polling.java
+++ ServerScheduling/src/scheduler/Polling.java
@@ -10,19 +10,37 @@ public class Polling extends PeriodicServer
 	}
 
 	@Override
-	public ArrayList<TaskInstance> doServer(ArrayList<TaskInstance> list) {
-		// TODO Auto-generated method stub
+	public ArrayList<TaskInstance> doServer(ArrayList<TaskInstance> list)
+	{
+
 		if(list.size() == 0)
 		{
 			return null;
 		}
 		
+		int sum=0;
+		ArrayList<TaskInstance> toRun = new ArrayList<TaskInstance>();
 		
-		return list;
+		for(int i=0;i<list.size();i++)
+		{
+			sum+=list.get(i).getCompTimeRemaining();
+			//Task time less than allocatable server time
+			if(sum <= totalCompTime)
+			{
+				toRun.add(list.get(i));
+			}
+			else
+			{
+				i=list.size();
+			}
+		}
+		return toRun;
 	}
 
 	@Override
-	public ArrayList<TaskInstance> update(ArrayList<TaskInstance> list) {
+	public ArrayList<TaskInstance> update(ArrayList<TaskInstance> list) 
+	{
+		//Polling doesn't do anything here
 		return null;
 	}
 	
